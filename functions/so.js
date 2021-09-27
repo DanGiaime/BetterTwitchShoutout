7f90b165f3d87d36dbc3d8e43110c8a47b4b2547
Rox Works <dsg6874@g.rit.edu>
2021-09-27 16:45:31 +0700

    only function

---
+++ functions/so.js
@@ -0,0 +1,21 @@
+// create a function that takes a username from the request body
+// calls the /helix/user endpoint from the twitch api
+// then returns the user's description
+// if the user doesn't exist, return a 404
+
+const axios = require('axios');
+exports.handler = async (event, context) => {
+    let username = event.queryStringParameters.username;
+    username = username.replace('@', '');
+    const url = `https://api.twitch.tv/helix/users?login=${username}`;
+    const response = await axios.get(url, {
+        headers: {
+            'Client-ID': process.env.CLIENT_ID,
+            'Authorization': `Bearer ${process.env.ACCESS_TOKEN}`
+        }
+    });
+    if (response.data.data.length === 0) {
+        return "They're awesome!";
+    }
+    return response.data.data[0].description;
+};
