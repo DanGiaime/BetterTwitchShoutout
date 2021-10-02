28f8a8bbb69f0a23455862a6eb43b7df6bf8151f
Rox Works <dsg6874@g.rit.edu>
2021-10-02 15:16:29 +0700

    Account for errors by telling people to copy the command again

--- functions/so.js
+++ functions/so.js
@@ -9,21 +9,39 @@ exports.handler = async (event, context) => {
     console.log(event.queryStringParameters.username);
     let username = event.queryStringParameters.username;
     username = username.replace('@', '');
-    const url = `https://api.twitch.tv/helix/users?login=${username}`;
-    const response = await axios.get(url, {
-        headers: {
-            'Client-ID': process.env.CLIENT_ID,
-            'Authorization': `Bearer ${process.env.ACCESS_TOKEN}`
-        }
-    });
+    username = username.trim();
+    username = username.split(' ')[0];
 
-    let userExists = response && response.data && response.data.data && response.data.data.length != 0;
-    if (!userExists) {
-        return { statusCode: 200, body: "They're awesome!"};
+    if(username.length < 1) {
+        return {
+            statusCode: 200,
+            body: "No username provided, so I'll shoutout my creator, check out https://twitch.tv/roxkstar74 :D"
+        }
     }
-    let descriptionExists = response.data.data[0] && response.data.data[0].description;
-    if(!descriptionExists) {
-        return { statusCode: 200, body: "They're awesome!"};
+    const url = `https://api.twitch.tv/helix/users?login=${username}`;
+    try {
+        const response = await axios.get(url, {
+            headers: {
+                'Client-ID': process.env.CLIENT_ID,
+                'Authorization': `Bearer ${process.env.ACCESS_TOKEN}`
+            }
+        });
+        let userExists = response && response.data && response.data.data && response.data.data.length != 0;
+        if (!userExists) {
+            console.log('returning default');
+            return { statusCode: 200, body: "They're awesome!"};
+        }
+        let descriptionExists = response.data.data[0] && response.data.data[0].description;
+        if(!descriptionExists) {
+            return { statusCode: 200, body: "They're awesome!"};
+        }
+        return { statusCode: 200, body: response.data.data[0].description};
+    } catch(error) {
+        return {
+            statusCode: 200,
+            body: "Either got a REALLY weird username, or you're using an old version of the @Roxkstar74 !so command. Please copy the updated command from this post: https://www.reddit.com/r/Twitch/comments/pwegd8/better_so_for_nightbot/"
+        }
     }
-    return { statusCode: 200, body: response.data.data[0].description};
+
+
 };
